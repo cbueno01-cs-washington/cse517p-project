cf43a99f47c55785f17c63641e41d19db59c030a
Chrystian Bueno <gizzmoo@Chrystians-MacBook-Pro.local>
2025-05-01 23:33:26 -0700

    update submit file to contain group name

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Chrystian Bueno,cbueno01" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
