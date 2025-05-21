6a03cd8b5683165e4322cc763c1add8d9e34740f
Chrystian Bueno <gizzmoo@Chrystians-MacBook-Pro.local>
2025-05-21 14:33:28 -0700

    Checkpoint 2

--- src/myprogram.py
+++ src/myprogram.py
@@ -3,6 +3,11 @@ import os
 import string
 import random
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import nltk
+import ast
+import json
+from nltk import ngrams
+from collections import defaultdict, Counter
 
 
 class MyModel:
@@ -10,11 +15,28 @@ class MyModel:
     This is a starter model to get you started. Feel free to modify this file.
     """
 
+    def __init__(self, model = defaultdict(lambda: defaultdict(lambda: 0))):
+        self.model = model
+        
     @classmethod
     def load_training_data(cls):
-        # your code here
-        # this particular model doesn't train
-        return []
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        parent_directory = os.path.dirname(current_directory)
+        training_dir = os.path.join(parent_directory, 'traindata')
+    
+        data = []
+        for file in os.listdir(training_dir):
+            filename = os.fsdecode(file)
+            if filename.endswith('.txt'):
+                with open(os.path.join(training_dir,filename), 'r', encoding='utf-8') as opened_file:
+                    try:
+                        content = opened_file.read()
+                        # Process the content
+                        data.extend(ngrams(content.lower(), 3))
+                    except:
+                        print("Could not open ", filename)
+
+        return data
 
     @classmethod
     def load_test_data(cls, fname):
@@ -23,7 +45,7 @@ class MyModel:
         with open(fname) as f:
             for line in f:
                 inp = line[:-1]  # the last character is a newline
-                data.append(inp)
+                data.append(inp[-2:])
         return data
 
     @classmethod
@@ -33,32 +55,49 @@ class MyModel:
                 f.write('{}\n'.format(p))
 
     def run_train(self, data, work_dir):
-        # your code here
-        pass
+        # Count bigrams
+        for w1, w2, w3 in data:
+            self.model[w1 + w2][w3] += 1
+
+        # Transform to probabilities
+        for w_seq in self.model:
+            total_count = float(sum(self.model[w_seq].values()))
+            for last_letter in self.model[w_seq]:
+                self.model[w_seq][last_letter] /= total_count
 
     def run_pred(self, data):
         # your code here
         preds = []
-        all_chars = string.ascii_letters
         for inp in data:
-            # this model just predicts a random character each time
-            top_guesses = [random.choice(all_chars) for _ in range(3)]
-            preds.append(''.join(top_guesses))
+            next_word_dict = self.model[inp.lower()]
+            line_pred = []
+
+            if next_word_dict:
+                counter = Counter(next_word_dict)
+                top3 = counter.most_common(3) 
+                line_pred = [item[0] for item in top3]
+            else:
+                all_chars = string.ascii_letters
+                line_pred = [random.choice(all_chars) for _ in range(3)]
+
+            preds.append(''.join(line_pred))
         return preds
 
     def save(self, work_dir):
         # your code here
         # this particular model has nothing to save, but for demonstration purposes we will save a blank file
         with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-            f.write('dummy save')
+            json.dump(self.model, f)
 
     @classmethod
     def load(cls, work_dir):
         # your code here
         # this particular model has nothing to load, but for demonstration purposes we will load a blank file
+        model = defaultdict(lambda: defaultdict(lambda: 0))
         with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            dummy_save = f.read()
-        return MyModel()
+            model.update(defaultdict(lambda: defaultdict(lambda: 0), json.load(f)))
+
+        return MyModel(model)
 
 
 if __name__ == '__main__':
